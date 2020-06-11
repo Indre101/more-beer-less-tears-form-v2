a836750b9b956b7a796498d38ffa810bf71d529c
indre101 <zygaityte.indre@gmail.com>
2020-06-11 19:17:16 +0200

    fixes lotti animation

--- src/components/Preloader.js
+++ src/components/Preloader.js
@@ -1,30 +1,21 @@
 // Logo Animation Lottie
-import React, { useRef } from "react";
-import Lottie from "react-lottie";
+import React, { useRef, useEffect } from "react";
 import animationData from "../assets/lotti/data08.json";
+import lottie from "lottie-web";
 import "../sass/partials/components/_preloader.scss";
 
 export default React.memo(function Preloader(props) {
-  const defaultOptions = {
-    loop: false,
-    autoplay: true,
-    animationData: animationData,
-    rendererSettings: {
-      preserveAspectRatio: "xMidYMid slice",
-      className: "lottie-anim-element",
-    },
-  };
+  useEffect(() => {
+    const animation = lottie.loadAnimation({
+      container: container.current,
+      renderer: "svg",
+      loop: true,
+      autoplay: false,
+      animationData: animationData,
+    });
+    animation.play();
+  }, []);
 
   const container = useRef();
-  return (
-    <div
-      className="preloader"
-      ref={container}
-      onAnimationEnd={() => {
-        container.current.style.display = "none";
-        props.setpreloaderPlayed(true);
-      }}>
-      <Lottie options={defaultOptions} height={350} width={350} />
-    </div>
-  );
+  return <div id="preloader" className="preloader" ref={container}></div>;
 });
